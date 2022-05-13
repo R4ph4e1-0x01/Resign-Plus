3ba9fd51c915d36d5fee61c5667203b2908e9267
fangzz <fangzz@knownsec.com>
2022-05-13 16:29:51 +0800

    v1.0

---
+++ src/main/java/custom/CRecalculater.java
@@ -0,0 +1,12 @@
+package custom;
+
+import java.util.Map;
+
+public class CRecalculater {
+
+	public String sign(Map<String, String> paraMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
